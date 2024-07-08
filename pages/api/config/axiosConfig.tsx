1a2b142fe3183bf0d85dd96d01f1ca7a27a34fca
arielferen <arieliten85@gmail.com>
2024-07-08 15:45:39 -0300

    add variable back production

--- pages/api/config/axiosConfig.tsx
+++ pages/api/config/axiosConfig.tsx
@@ -1,7 +1,13 @@
 import { checkTokenExpiration, getTokenStorage } from "@/utils/tokenService";
-import axios from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_DEV;
+interface CustomAxiosInstance extends AxiosInstance {
+  (config: AxiosRequestConfig): Promise<any>;
+}
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_PROD;
+
+console.log(BASE_URL);
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -10,11 +16,13 @@ const axiosInstance = axios.create({
   },
 });
 
+// EN CADA CONSULTA CHEQUEA QUE TENGA EL TOKEN Y NO HAYA EXPIRADO
 axiosInstance.interceptors.request.use(
   (config) => {
     checkTokenExpiration();
     const token = getTokenStorage();
-    if (token) {
+
+    if (token && config.url && !config.url.startsWith(`${BASE_URL}/auth/`)) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
