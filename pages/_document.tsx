d5c5f981f7cc76605adc777d59da76f36a99864d
arielferen <arieliten85@gmail.com>
2024-07-10 13:26:09 -0300

    reactor text styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=PT+Sans+Narrow:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
